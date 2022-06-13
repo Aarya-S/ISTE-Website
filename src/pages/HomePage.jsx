2d2b1a90199efea9085dc4275e6e785c52a35686
Saket Nandanwar <70093885+whodissaket@users.noreply.github.com>
2022-06-13 16:03:42 +0530

    Added: Footer with Map iframe
    Map not responsive yet

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import Navbar2 from '../components/Navbar2';
-
+import React from "react";
+import Footer from "../components/Footer/Footer";
+import Navbar2 from "../components/Navbar2";
 
 const HomePage = () => {
   return (
     <>
       <Navbar2 />
       <h1>Homepage</h1>
+      <Footer />
     </>
-    
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
