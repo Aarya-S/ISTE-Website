8b8df7c5539ddda56837a028049ed9c9d3eb000d
Aditya bhise <88058333+aaddiittyyaaa@users.noreply.github.com>
2022-06-21 00:10:57 +0530

    added blog header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../css/Header.css';
+// import rossi from '../assets/rossi.jpg';
+
+function Header() {
+  return (
+    <div className="Custom-Header" >
+      
+      <p className='blog-title'>welcome to iste-rait</p>
+      <p className='blog-author'>sheru</p>
+      <p className='blog-date'>19 June 2022</p>
+      
+    </div>
+  )
+}
+
+export default Header
